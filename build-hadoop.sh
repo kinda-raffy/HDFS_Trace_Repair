47d7c0f25d5990df4072d9f7758a10a3bf71329a
Liviru <132646852+LiviruRMIT@users.noreply.github.com>
2024-07-07 20:01:31 +1000

    Add vscode dev container

--- build-hadoop.sh
+++ build-hadoop.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
-mvn package -Pdist,native -DskipTests -Dmaven.javadoc.skip
+docker run --name hadoop-build --rm -itd \
+    -v "./:/home/hadoop/hdfs-trace-repair" \
+    -v "${HOME}/.m2:/home/hadoop/.m2"  \
+    -h hadoop-build \
+    hadoop-build
+
+docker exec hadoop-build mvn package -Pdist,native -Dtar -DskipTests -Dmaven.javadoc.skip
+
+docker stop hadoop-build 
