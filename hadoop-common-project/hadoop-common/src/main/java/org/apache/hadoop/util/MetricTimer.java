45b2d68b83eb47ad7e12998806508fa4eb6da5a4
Liviru <132646852+LiviruRMIT@users.noreply.github.com>
2024-08-26 11:17:20 +1000

    Remove writing summaries

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/MetricTimer.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/MetricTimer.java
@@ -1,36 +1,28 @@
 package org.apache.hadoop.util;
 
-import com.sun.jndi.ldap.LdapURL;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
 
 public class MetricTimer implements AutoCloseable {
     private static final ThreadLocal<Deque<Long>> startTimes = ThreadLocal.withInitial(LinkedList::new);
     private static final ThreadLocal<Deque<Long>> startWallTimes = ThreadLocal.withInitial(LinkedList::new);
-    private final ConcurrentHashMap<String, List<Long>> recordedMetrics = new ConcurrentHashMap<>();
     private final ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
     private final String metric;
-    private final String summaryFile;
     private final FileWriter logFileWriter;
 
     public MetricTimer(String metric) {
         this.metric = metric;
         String logDir = "Benchmark";
-        String summaryDir = "Benchmark/Summary";
         String logFile = String.format("%s/%s.log", logDir, metric);
-        this.summaryFile = String.format("%s/%s.summary.log", summaryDir, metric);
 
         try {
             Files.createDirectories(Paths.get(logDir));
-            Files.createDirectories(Paths.get(summaryDir));
             this.logFileWriter = new FileWriter(logFile);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -58,7 +50,6 @@ public class MetricTimer implements AutoCloseable {
                 long duration = threadMXBean.getCurrentThreadCpuTime() - start;
                 long durationWL = System.currentTimeMillis() - startWL;
                 writeLog(label, duration, durationWL);
-                recordedMetrics.computeIfAbsent(label, k -> new ArrayList<>()).add(duration);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -95,41 +86,7 @@ public class MetricTimer implements AutoCloseable {
         }
     }
 
-    private void writeMetricSummary() {
-        try (FileWriter summaryFw = new FileWriter(summaryFile)) {
-            String dashLine = new String(new char[50]).replace("\0", "-");
-            summaryFw.write(dashLine + "\n");
-            summaryFw.write(metric + "'s Operation Summary\n");
-            summaryFw.write(dashLine + "\n\n");
-
-            for (Map.Entry<String, List<Long>> entry : recordedMetrics.entrySet()) {
-                String label = entry.getKey();
-                List<Long> durations = entry.getValue();
-                double average_ns = durations.stream().mapToDouble(Long::doubleValue).average().orElse(0);
-                long min_ns = durations.stream().min(Long::compare).orElse(0L);
-                long max_ns = durations.stream().max(Long::compare).orElse(0L);
-                double total_ns = durations.stream().mapToDouble(Long::doubleValue).sum();
-                double average_sec = average_ns / 1_000_000_000.0;
-                double min_sec = min_ns / 1_000_000_000.0;
-                double max_sec = max_ns / 1_000_000_000.0;
-                double total_sec = total_ns / 1_000_000_000.0;
-
-                summaryFw.write(String.format("%s\n", label));
-                summaryFw.write(String.format(
-                        "\tOccurrence: %d\n\tSum: %.2f ns (%.6f s)" +
-                                "\n\tAvg: %.2f ns (%.6f s)\n\tMin: %d ns (%.6f s)" +
-                                "\n\tMax: %d ns (%.6f s)\n\n",
-                        durations.size(), total_ns, total_sec,
-                        average_ns, average_sec, min_ns, min_sec, max_ns, max_sec)
-                );
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     @Override
-    public void close() {
-        writeMetricSummary();
+    public void close() throws Exception {
     }
 }
