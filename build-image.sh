154e123066d261c0ae7dcb30f04485457c1a1e40
Liviru <132646852+LiviruRMIT@users.noreply.github.com>
2024-11-18 08:24:57 +1100

    Remove unused scripts

--- build-image.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker build \
-    -t hadoop-build \
-    -f dev-support/docker/Dockerfile_aarch64 dev-support/docker
--- start-build-env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-docker run --name hadoop-build --rm -it \
-    -v "./:/home/hadoop/hdfs-trace-repair" \
-    -v "${HOME}/.m2:/home/hadoop/.m2"  \
-    -p "5005:5005" \
-    -h hadoop-build \
-    --network hadoop \
-    hadoop-build
